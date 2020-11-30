2cc9db113f49accc0fefd9762e59c21ddf26c637
Kreuzritterrr <75267598+Kreuzritterrr@users.noreply.github.com>
2020-11-30 20:03:52 +0200

    Add files via upload

---
+++ Conus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ooplab2
+{
+
+    class Conus
+    {
+        public static double r1 { get; set; }
+        public static double r2 { get; set; }
+        public static double h { get; set; }
+        public void Parametrs()
+        {
+            Console.WriteLine("Введите больший радиус усечённого конуса\n r1=");
+            r1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите меньший радиус усечённого конуса\n r2=");
+            r2 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите высоту усечённого конуса");
+            h = Double.Parse(Console.ReadLine());
+        }
+        public void Result()
+        {
+            double V = (Math.PI * h * (r1 * r1 + r1 * r2 + r2 * r2)) / 3;
+            Console.WriteLine("Объём усечённого конуса = {0}", V);
+        }
+        ~Conus()
+        {
+            Console.WriteLine("oh no");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ooplab2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Усечённый конус");
+            var con = new Conus();
+            con.Parametrs(); con.Result();
+
+            Console.ReadLine();
+        }
+    }
+}
